938e9b6062e4edacc7b1bb2f9072ddd18e17c488
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-23 01:45:53 +0300

    Create framework detail view

---
+++ Apple-Frameworks-SwiftUI/FrameworkDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/23/24.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FrameworkDetailView()
+}
