8b6ede27221cced6c2f7c027b2367762c6c478fa
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-23 02:49:59 +0300

    Add framework detail view

--- Apple-Frameworks-SwiftUI/FrameworkDetailView.swift
+++ Apple-Frameworks-SwiftUI/FrameworkDetailView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct FrameworkDetailView: View {
+    var framework: Framework
+    @Binding var isShowingDetailView: Bool
+    @State private var isShowingSafariView = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                Spacer()
+                Button{
+                    isShowingDetailView.toggle()
+                }label: {
+                    Image(systemName: "xmark")
+                        .foregroundStyle(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44,height: 44)
+                }
+            }.padding()
+            Spacer()
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            Spacer()
+            Button{
+                isShowingSafariView = true
+            }label: {
+                AFButton(title: "Learn More")
+                
+            }.sheet(isPresented: $isShowingSafariView){
+                SafariView(url: URL(string: framework.urlString) ?? URL(string: "www.apple.com")!)
+            }
+        }
     }
 }
 
 #Preview {
-    FrameworkDetailView()
+    FrameworkDetailView(framework: MockData.sampleFramework,isShowingDetailView: .constant(false))
 }
