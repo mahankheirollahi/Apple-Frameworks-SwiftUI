0c3f233b591a6166eaec32b701419179519870c1
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-23 01:45:25 +0300

    Add gridview

--- Apple-Frameworks-SwiftUI/FrameworkGridView.swift
+++ Apple-Frameworks-SwiftUI/FrameworkGridView.swift
@@ -8,19 +8,38 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    let columns: [GridItem] = [GridItem(.flexible()),GridItem(.flexible()),GridItem(.flexible())]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks){ framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }.navigationTitle("🍎 Frameworks")
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     FrameworkGridView()
+}
+
+struct FrameworkTitleView: View {
+    let framework: Framework
+    var body: some View{
+        VStack {
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 90,height: 90)
+            Text(framework.name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6)
+        }.padding()
+    }
 }
 
 
