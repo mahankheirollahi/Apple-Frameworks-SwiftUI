75f0b83a871a102da91a69199c96c5d18a488f81
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-23 01:11:32 +0300

    Initial Commit

---
+++ Apple-Frameworks-SwiftUI/Apple_Frameworks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_SwiftUIApp.swift
+//  Apple-Frameworks-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Apple-Frameworks-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Apple-Frameworks-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
